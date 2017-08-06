a84c00179e9f9c326c97f3bef4c35ee7184a934e
Melissa <Melissa@toshiba>
2017-08-06 18:59:12 -0500

    G

---
+++ ComplejoDeportivo/src/complejodeportivo/Imagen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package complejodeportivo;
+
+/**
+ *
+ * @author Melissa
+ */
+public class Imagen {
+ public static void main(String[] args) {
+        // TODO code application logic here
+     MenuMain menu=new MenuMain();
+     menu.setVisible(true);
+    }
+}
